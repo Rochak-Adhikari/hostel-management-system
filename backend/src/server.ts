b946f833e4c812d492c3e798031f9b8122c78743
Rochak-Adhikari <bossrk586@gmail.com>
2026-06-19 17:26:50 +0545

    express server createed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,22 @@
 import express, { Request, Response } from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-
-import connectDB from "./config/db";
-
-dotenv.config();
 
 const app = express();
+const PORT = 8000;
 
-// Connect Database
-connectDB();
+//* connect to database
 
-app.use(cors());
-app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("HostelHub API Running");
+//*using middleware
+
+
+//root route
+app.get("/", (req:Request, res:Response) => {  
+  res.status(200).json({ 
+    message: "Welcome to HostelHub API" ,
+  });  
 });
-
-const PORT = process.env.PORT || 5000;
-
+//listen
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`press CTRL + C to stop the server`);
 });
